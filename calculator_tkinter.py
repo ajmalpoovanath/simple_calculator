c8d2e60b4d752926fa5d799a3afe611585f2e5d9
Ajmal <ajmal.poovanath@gmail.com>
2022-07-27 19:41:23 +0530

    calculator window created

--- calculator_tkinter.py
+++ calculator_tkinter.py
@@ -1,5 +1,11 @@
 # before running this programme please install tkinter on your system
+import tkinter
+from logging import root
 from tkinter import *
-
 window = Tk()
+window.geometry("570x600")
+window.title("Calculator")
+window.resizable(False,False)
+window.configure(bg="#B1B1B1")
+
 window.mainloop()
